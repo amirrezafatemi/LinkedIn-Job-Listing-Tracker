70a69ff94b1a046a4cfc47bc79d471edeaa8a86e
AmirRezaFatemi <71829374+amirrezafatemi@users.noreply.github.com>
2025-06-01 19:23:00 +0430

    UI designed 02

--- layout/login.py
+++ layout/login.py
@@ -2,11 +2,14 @@ import streamlit as st
 
 
 st.title("LinkedIn Job Listing Tracker")
+with st.form("Login page"):
 
-email = st.text_input("Email", icon=":material/mail:")
-password = st.text_input("Password", type="password", icon=":material/key:")
+    email = st.text_input("Email", icon=":material/mail:")
+    password = st.text_input("Password", type="password", icon=":material/key:")
 
+    st.form_submit_button(label="Submit")
 
+st.write("outside")
 # st.page_link("app.py", label="The App", icon=":material/Login:")
 
 
