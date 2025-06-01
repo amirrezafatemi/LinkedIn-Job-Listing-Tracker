3849e6185d591073383bb11ba0810ead66634b2d
AmirRezaFatemi <71829374+amirrezafatemi@users.noreply.github.com>
2025-06-01 19:11:12 +0430

    UI designed 01

---
+++ layout/login.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.title("LinkedIn Job Listing Tracker")
+
+email = st.text_input("Email", icon=":material/mail:")
+password = st.text_input("Password", type="password", icon=":material/key:")
+
+
+# st.page_link("app.py", label="The App", icon=":material/Login:")
+
+
+
+
+
+
+
+
